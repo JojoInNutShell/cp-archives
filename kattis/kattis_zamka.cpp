ed55c8df3039f4b62ce0c74e3a9d072f6e503f7d
MikiMiku <63213833+JojoInNutShell@users.noreply.github.com>
2022-12-23 21:49:22 +0700

    Add files via upload

---
+++ kattis/kattis_zamka.cpp
@@ -0,0 +1,92 @@
+/*
+	Author: MikiMiku
+	
+	Idea: 
+		As the constraint is known to be quite
+		small for brute forcing. Then, we will
+		just do it that way.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define int long long
+#define endl '\n'
+
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define pb push_back 
+#define eb emplace_back 
+#define mp make_pair
+
+#define isOn(S, j) (S & (1 << j))
+#define setBit(S, j) (S |= (1 << j))
+#define clearBit(S, j) (S &= ~(1 << j))
+#define toggleBit(S, j) (S ^= (1 << j))
+#define LSB(S) (S & (-S))
+#define setAll(S, n) (S = (1 << n) - 1)
+#define modulo(S, N) ((S) & (N - 1))   // returns S % N, where N is a power of 2
+#define isPowerOfTwo(S) (!(S & (S - 1)))
+#define nearestPowerOfTwo(S) ((int)pow(2.0, (int)((log((double)S) / log(2.0)) + 0.5)))
+#define turnOffLastBit(S) ((S) & (S - 1))
+#define turnOnLastZero(S) ((S) | (S + 1))
+#define turnOffLastConsecutiveBits(S) ((S) & (S + 1))
+#define turnOnLastConsecutiveZeroes(S) ((S) | (S - 1))
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const int FMOD = 998244353; 
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+const double PI=acos(-1);
+
+#define fi first
+#define se second
+typedef pair<int, int> ii;  
+typedef vector<ii> vii;
+typedef vector<int> vi;
+typedef map<int,int> mii; 
+
+//FLOAT PRECISION SETTINGS
+/*
+cout.setf(ios::fixed,ios::floatfield);
+cout.precision(3);
+*/
+//FILE IO
+void setIO(string s) {
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+
+//........................................................................
+
+int count(int n) {
+	int c = 0;
+	while(n != 0) {
+		c += (n%10);
+		n /= 10;
+	}
+	return c;
+}
+
+signed main() {
+	ios_base::sync_with_stdio(0);cin.tie(nullptr); cout.tie(nullptr);
+	int L, R, sumD;
+	cin >> L >> R >> sumD;
+	
+	int mn = INF, mx = 0;
+	for(int i = L; i <= R; ++i) {
+		if(count(i) == sumD) {
+			mn = min(mn, i);
+			mx = max(mx, i);
+		}
+	}
+	
+	cout << mn << endl << mx << endl;
+	return 0;
+}
